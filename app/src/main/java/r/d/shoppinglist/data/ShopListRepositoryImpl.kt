fb5014b7a3e2fecef72b7c0fb0798560b9c7c435
SolomonVP <Lum7023@yandex.ru>
2024-06-03 23:34:55 +0300

    test bag for RecyclerView

--- app/src/main/java/r/d/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/r/d/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import r.d.shoppinglist.domain.ShopItem
 import r.d.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
+import kotlin.random.Random
 
 object ShopListRepositoryImpl: ShopListRepository {
 
@@ -14,8 +15,8 @@ object ShopListRepositoryImpl: ShopListRepository {
     private var autoIncrementId = 0
 
     init {
-        for (i in 0 until 100) {
-            val item = ShopItem("Name $i", i, true)
+        for (i in 0 until 1000) {
+            val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
