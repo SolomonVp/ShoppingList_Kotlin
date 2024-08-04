0134d07a7835b23022973a96c2c1ed60c80eb6c3
SolomonVP <Lum7023@yandex.ru>
2024-08-04 18:13:13 +0300

    + Mappers

---
+++ app/src/main/java/r/d/shoppinglist/data/ShopListMapper.kt
@@ -0,0 +1,25 @@
+package r.d.shoppinglist.data
+
+import r.d.shoppinglist.domain.ShopItem
+
+class ShopListMapper {
+
+    fun mapEntityToDbModel(shopItem: ShopItem): ShopItemDbModel = ShopItemDbModel(
+        id = shopItem.id,
+        name = shopItem.name,
+        count = shopItem.count,
+        enabled = shopItem.enabled
+    )
+
+    fun mapDbModelToEntity(shopItemDbModel: ShopItemDbModel) = ShopItem(
+        id = shopItemDbModel.id,
+        name = shopItemDbModel.name,
+        count = shopItemDbModel.count,
+        enabled = shopItemDbModel.enabled
+    )
+
+    fun mapListDbModelToListEntity(list: List<ShopItemDbModel>) = list.map {
+        mapDbModelToEntity(it)
+    }
+
+}
